29905115a31e84993652a9e1a129f7dddf46e748
Mehran Bayat <157355713+mehraxn@users.noreply.github.com>
2025-08-28 15:26:06 +0200

    update

---
+++ Codes description/k (Graph Basics)/a) (Representation)/d) Graph Basics (Representation  PDF , Page  10 & 11 & 12 & 13 & 14 & 15)/Complete.c
@@ -0,0 +1,157 @@
+#include <stdlib.h>   // malloc, calloc, free
+#include <stdio.h>    // FILE, fopen, fgets, sscanf, fclose, perror
+#include <limits.h>   // INT_MAX
+#include <string.h>   // memset
+
+#define MAX_LINE 100
+
+enum { WHITE, GREY, BLACK };
+
+typedef struct graph_s graph_t;
+typedef struct vertex_s vertex_t;
+
+struct graph_s {
+    vertex_t *g;
+    int nv;
+};
+
+struct vertex_s {
+    int id;
+    int color;
+    int dist;
+    int disc_time;
+    int endp_time;
+    int pred;
+    int scc;
+    int *rowAdj;
+};
+
+
+graph_t *graph_load(char *filename);
+void graph_attribute_init(graph_t *g);
+int graph_find(graph_t *g, int id);
+void graph_dispose(graph_t *g);
+
+// void *util_calloc(size_t n, size_t size);
+// FILE *util_fopen(const char *filename, const char *mode);
+// void graph_print(graph_t *g);
+// void graph_add_edge(graph_t *g, int u, int v, int weight, int directed);
+// void graph_remove_edge(graph_t *g, int u, int v, int directed);
+
+
+graph_t *graph_load(char *filename) {
+    graph_t *g;
+    char line[MAX_LINE];
+    int i, j, weight, dir;
+    FILE *fp;
+
+    g = util_calloc(1, sizeof(graph_t));
+
+    fp = util_fopen(filename, "r");
+    fgets(line, MAX_LINE, fp);
+    if (sscanf(line, "%d%d", &g->nv, &dir) != 2) {
+        sscanf(line, "%d", &g->nv);
+        dir = 1;
+    }
+
+    g->g = util_calloc(g->nv, sizeof(vertex_t));
+
+    for (i = 0; i < g->nv; i++) {
+        g->g[i].id = i;
+        g->g[i].color = WHITE;
+        g->g[i].dist = INT_MAX;
+        g->g[i].pred = -1;
+        g->g[i].scc = -1;
+        g->g[i].disc_time = -1;
+        g->g[i].endp_time = -1;
+        g->g[i].rowAdj = util_calloc(g->nv, sizeof(int));
+    }
+
+    while (fgets(line, MAX_LINE, fp) != NULL) {
+        if (sscanf(line, "%d%d%d", &i, &j, &weight) != 3) {
+            sscanf(line, "%d%d", &i, &j);
+            weight = 1;
+        }
+        g->g[i].rowAdj[j] = weight;
+        if (dir == 0) {
+            g->g[j].rowAdj[i] = weight;
+        }
+    }
+    fclose(fp);
+    return g;
+}
+
+void graph_attribute_init(graph_t *g) {
+    for (int i = 0; i < g->nv; i++) {
+        g->g[i].color = WHITE;
+        g->g[i].dist = INT_MAX;
+        g->g[i].disc_time = -1;
+        g->g[i].endp_time = -1;
+        g->g[i].pred = -1;
+        g->g[i].scc = -1;
+    }
+}
+
+int graph_find(graph_t *g, int id) {
+    for (int i = 0; i < g->nv; i++) {
+        if (g->g[i].id == id) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void graph_dispose(graph_t *g) {
+    for (int i = 0; i < g->nv; i++) {
+        free(g->g[i].rowAdj);
+    }
+    free(g->g);
+    free(g);
+}
+
+
+// void *util_calloc(size_t n, size_t size) {
+//     void *p = calloc(n, size);
+//     if (!p) {
+//         perror("calloc failed");
+//         exit(EXIT_FAILURE);
+//     }
+//     return p;
+// }
+
+// FILE *util_fopen(const char *filename, const char *mode) {
+//     FILE *fp = fopen(filename, mode);
+//     if (!fp) {
+//         perror("fopen failed");
+//         exit(EXIT_FAILURE);
+//     }
+//     return fp;
+// }
+
+
+// void graph_print(graph_t *g) {
+//     printf("Adjacency Matrix:\n");
+//     for (int i = 0; i < g->nv; i++) {
+//         for (int j = 0; j < g->nv; j++) {
+//             printf("%d ", g->g[i].rowAdj[j]);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// void graph_add_edge(graph_t *g, int u, int v, int weight, int directed) {
+//     if (u >= g->nv || v >= g->nv) return;
+//     g->g[u].rowAdj[v] = weight;
+//     if (!directed) {
+//         g->g[v].rowAdj[u] = weight;
+//     }
+// }
+
+// void graph_remove_edge(graph_t *g, int u, int v, int directed) {
+//     if (u >= g->nv || v >= g->nv) return;
+//     g->g[u].rowAdj[v] = 0;
+//     if (!directed) {
+//         g->g[v].rowAdj[u] = 0;
+//     }
+// }
+
